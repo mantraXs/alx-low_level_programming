1073ac2178cab1d346e995073e10ec05b21314f0
Hudhayfah <hoismail2017@gmail.com>
2022-07-01 11:07:37 -0700

    q3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a == 'q' || a == 'e')
+			continue;
+		else
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
