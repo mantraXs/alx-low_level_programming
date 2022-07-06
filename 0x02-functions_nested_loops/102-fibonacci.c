00f3b434fe4cbf77f7cb6d0e3fcb4a1e3efa9c5d
Hudhayfah <hoismail2017@gmail.com>
2022-07-05 21:24:33 -0700

    q102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	long num1 = 1;
+	long num2 = 2;
+	long r;
+	int counter;
+
+	for (counter = 0; counter <= 48; counter++)
+	{
+		if (counter == 48)
+		{
+			printf("%ld", r);
+			break;
+		}
+		else if (num1 == 1 && num2 == 2)
+		{
+			printf("%ld, ", num1);
+			printf("%ld, ", num2);
+			r = num1 + num2;
+			num1 = num2;
+			num2 = r;
+		}
+		else
+		{
+			printf("%ld, ", r);
+			r = num1 + num2;
+			num1 = num2;
+			num2 = r;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
