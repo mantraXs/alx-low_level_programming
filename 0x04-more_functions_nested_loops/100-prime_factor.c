8ca43126c69d6d6608d7e113ba482728975d3492
Hudhayfah <hoismail2017@gmail.com>
2022-07-08 20:43:00 -0700

    q100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - find largest factor of 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	long n = 612852475143;
+	long i = 2;
+
+	while (i < (n / 2))
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			i = 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}
