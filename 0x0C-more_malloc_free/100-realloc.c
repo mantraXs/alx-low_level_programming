bba5f780637ca7f72bf2303a8c95ebf8347ba263
Hudhayfah <hoismail2017@gmail.com>
2022-07-28 11:04:05 -0700

    q100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 
 /**
  * _realloc - reallocates a memory block using malloc and free
- * @*ptr: pointer to the memory previously allocated
+ * * @ptr: pointer to the memory previously allocated
  * @old_size: size in bytes of ptr
  * @new_size: new size in bytes of the new memory block
  * Return: pointer to the new memory block
