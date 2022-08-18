aff38865702bfdfc2b318cf439c268c7ec309d9e
Hudhayfah <hoismail1430@gmail.com>
2022-08-18 09:05:34 -0700

    q3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * set_bit - set the bit at a given index
- * @n: number to check
- * @index: index to check
- * Return: bit value or -1;
+ * set_bit - func that sets the value of a bit to 1 at a given index
+ * @n: number ot be processed
+ * @index: index to set the value
+ *
+ * Return: 1 if it worked or -1 otherwise
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i = 1, len = 0;
+	unsigned long int size;
+	unsigned long int mask;
 
-	if (!n)
+	size = sizeof(*n) * 8 - 1;
+	if (index > size)
+	{
 		return (-1);
-	while (len++ < index)
-		i = i << 1;
-	*n |= i;
+	}
+	mask = 1 << index;
+	*n = mask | *n;
 	return (1);
 }
